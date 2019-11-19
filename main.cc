422b5ef0b6440bd2bc1895eca9232e5249604240
Jordan <jordantaylorlewis@gmail.com>
2019-11-19 01:20:44 -0500

    Update main.cc
    This is a very cleaned up version of the code.
    -Removed the median sort, it took a very long time for larger arrays.
    -Removed testing results from end of code
    -Cleaned up some formatting when printing results to console
    This is a little quicker and easier for comparing with different array sizes. I did notice that it gets a segmentation fault at 10,000,000 elements but not 1,000,000. I am still looking into that. The quicksort runs fine, but crashes when it needs to radix sort. Further testing shows that it works fine with n=2,092,500 but not n=2,100,000

--- main.cc
+++ main.cc
@@ -11,6 +11,7 @@
 #include <cstdlib>
 #include <ctime>
 #include <sys/time.h>
+#include <fstream>
 
 using namespace std;
 
@@ -20,50 +21,23 @@ using namespace std;
 void printArray(int a[], int size);
 void randomizeArray (int a[], int size);
 void quickSort(int a[], int first, int last);
-<<<<<<< Updated upstream
-bool isSorted(int a[], int size);
-
-=======
 void radixSort(int a[], int n);
 void countSort(int a[], int n, int exp);
 void isSorted(int a[], int size);
 int max(int a[], int n);
->>>>>>> Stashed changes
 /************************  M A I N   F U N C T I O N  **********************/
 
 int main()
 {
-    //initialize variables
+
     int arraySize;
     struct timeval before, after;
-<<<<<<< Updated upstream
-    double timing;
-=======
     double timing, timingQuick, timingRadix;
->>>>>>> Stashed changes
 
-    //ask the user for the size of the array
+    // ask the user for the size of the array
     cout << "Please enter the size of the array." << endl;
     cin >> arraySize;
 
-<<<<<<< Updated upstream
-    //allocate a dynamic array of the appropriate size
-    int* arr=NULL;
-    arr = new int[arraySize];
-   
-    //input all the elements of the array from the
-    //cin (I will use redirection from a file for the test)
-    //inputArray(arr, arraySize);
-
-    srand (time(NULL));                              //initializes random seed
-    randomizeArray(arr, arraySize);                  //randomly generates numbers to populate
-    gettimeofday (&before, 0);                       //set beginning time
-    
-    //sort array
-    quickSort(arr, 0, arraySize-1);
-    
-    gettimeofday (&after, 0);                        //set ending time
-=======
     // allocate a dynamic array of the appropriate size
     int* arr = NULL;            // Pointer to an int, initialized to NULL.
     int* arr2 = NULL;
@@ -116,34 +90,12 @@ int main()
     gettimeofday (&before, 0);
     radixSort(arr2, arraySize);
     gettimeofday (&after, 0);
->>>>>>> Stashed changes
     timing = (double) ((double)after.tv_sec +
              (double)after.tv_usec/(1000*1000)) -
              (double) ((double)before.tv_sec +
-	     (double)before.tv_usec/(1000*1000));    //calculate total time
+             (double)before.tv_usec/(1000*1000));
+    timingRadix = timing;
 
-<<<<<<< Updated upstream
-    //output the sorted array;
-    cout << endl;
-    cout << "Here is the sorted array: " << endl;
-    printArray(arr, arraySize);
-
-    //test if the array is sorted, just in case
-    cout << "\n" << "\n";
-    if (isSorted(arr, arraySize))
-        cout << "Sorted." << endl;
-    else
-        cout << "Not sorted." << endl;
-
-    //output the timing information.
-    cout << "\n" << "\n";
-    cout << "It took " << timing << " seconds to sort the array." << endl;
-
-    delete []arr;
-    arr=NULL;
-    return 0;
-}//main()
-=======
     //cout << endl;
     //printArray(arr, arraySize);
 
@@ -166,63 +118,57 @@ int main()
     arr2 = NULL;
     return 0; // Indicate normal termination.
 } // main()
->>>>>>> Stashed changes
 
 
 
 
 /***************** A D D I T I O N A L  F U N C T I O N S ********************/
+// prints array
 void printArray(int a[], int size)
 {
     for (int i = 0; i<size; ++i)
-    {
-      cout << a[i] << ", ";                          //print element
-    }
-}//printArray()
+        cout << a[i] << ", ";
+}// print()
 
 
-
-//Using a randomize function to generate numbers to fill array
+// use a random number generators to fill array
 void randomizeArray (int a[], int size)
 {
     srand((unsigned)time(0));
 
-    //Using RAND_MAX, a constant in cstdlib guaranteed to be at
-    //least32767 for generating random numbers. 
     for (int i=0; i<size; ++i)
+        // Using RAND_MAX, a constant in cstdlib guaranteed at to be at least
+        // 32767 for generating random numbers. 
         a[i]=(rand()%RAND_MAX);   
-}//randomizeArray()
+}// randomizeArray()
 
 
-//Quick Sort has average complexity best case O(n log2 n) and worst case
-// is O(n^2).
+// Quick Sort has average complexity best case O(n log2 n)
+// and worst case is O(n^2).
 void quickSort(int a[], int first, int last)
 {
-    if (last <= first)                       //check if size is larger than zero
+    if (last <= first)           //base case arrays <= 1 in length are sorted
         return;
+  
     int pivot = a[first];
-    int i = first + 1, j = last;             //Initialize traversing variables
-     
-    while (i < j)                            //loop until index i and j converge
-    {
-         while (a[i] < pivot && i<j)
-	     i++;                     // finding an object at least as large
-         while (a[j] > pivot)             // as a[first].
-             j--;
-         if (i < j)
-	   swap(a[i++], a[j--]);
-    }
+    int i = first+1, j = last;
 
+    while (i < j)
+    {
+        while (a[i] < pivot && i<j)
+	    i++;
+        while (a[j] > pivot)
+	    j--;
+        if (i < j)
+	    swap(a[i++], a[j--]);
+    }
     if (a[j] > pivot)
-        j--;
-    swap (a[j], a[first]);
+	j--;
+    swap(a[j], a[first]);  
     quickSort(a, first, j-1);
     quickSort(a, j+1, last);
 }// quickSort()
 
-<<<<<<< Updated upstream
-bool isSorted(int a[],int size)
-=======
 void radixSort(int a[], int n)
 {
     int m = max(a, n);
@@ -232,37 +178,41 @@ void radixSort(int a[], int n)
 }// radixSort()
 
 void countSort(int a[], int n, int exp)
->>>>>>> Stashed changes
 {
-    for (int i=0; i<size-1; i++)
+    int output[n];
+    int i, count[10] = {0};
+
+    for (i = 0; i<n; i++)
+        count[(a[i]/exp)%10]++;
+    for (i = 1; i<10; i++)
+        count[i] += count[i-1];
+    for (i = n-1; i >= 0; i--)
     {
-        if (a[i] > a[i+1])
-            return false;
+        output[count[(a[i]/exp)%10]-1] = a[i];
+        count[(a[i]/exp)%10]--;
     }
-    return true;
-}
-<<<<<<< Updated upstream
+    for (i = 0; i<n; i++)
+        a[i] = output[i];
+    return;
+}// countSort()
 
-/*Including the code for Binary Insertion Sort just in case.
-  Will need to add to prototypes if decide to use it!
- 
-//Binary Insertion Sort best case is: O(n log2 n).
-
-void binaryInsertionSort (int arrayA[], int size)
+void isSorted(int a[], int size)
 {
-    for (int p = 1; p<size; ++p)
-      {
-        bool found;
-        int k;
-        binarySearch(a, a[p], 0, p-1, found, k); //possibly binary_search as in                                                    CSTL library...
-        int temp = a[p];
-        for (int i = p-1; i>=k; --i)
-	a[i+1]=a[i];
-      a[k] = temp;
-    }
+    for (int i = 0; i<size-1; i++)
+        if (a[i] > a[i+1])
+        {
+            cout << "The array is not sorted correctly." << endl;
+            return;
+        }
+    cout << "The array is sorted." << endl;
+    return;
+}// isSorted()
 
-
-}//binaryInsertionSort()
-*/
-=======
->>>>>>> Stashed changes
+int max(int a[], int n)
+{
+    int max = 0;
+    for (int i = 0; i<n; i++)
+        if (a[i] > max)
+            max = a[i];
+    return max;
+}
